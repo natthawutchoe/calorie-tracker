1fa4d1a0e62a8f67024d85db3c9d0a567ef44c89
natthawutchoe <natthawut.choe@ku.th>
2026-08-05 17:44:19 +0700

    Update sw.jsBump cache to v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "calorie-tracker-v13";
+const CACHE_NAME = "calorie-tracker-v14";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
