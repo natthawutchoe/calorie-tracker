f666972da79937e679d0c72e9d4cb6770d5b9de4
natthawutchoe <natthawut.choe@ku.th>
2026-08-09 14:36:18 +0700

    Bump app cache to v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "calorie-tracker-v19";
+const CACHE_NAME = "calorie-tracker-v20";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
