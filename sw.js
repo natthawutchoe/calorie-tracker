23df81caaee3c9b38c1d0adeee836a7fbd4a4a0f
natthawutchoe <natthawut.choe@ku.th>
2026-09-01 11:27:26 +0700

    Bump cache for dashboard

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "calorie-tracker-v23";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "calorie-tracker-v24";
+const ASSETS = ["./", "./index.html", "./dashboard.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
