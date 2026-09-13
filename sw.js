5930b3e7edf2296c95b6b609668c90a996a13d35
natthawutchoe <natthawut.choe@ku.th>
2026-09-13 14:16:07 +0700

    Bump cache to v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "calorie-tracker-v27";
+const CACHE_NAME = "calorie-tracker-v28";
 const ASSETS = ["./", "./index.html", "./dashboard.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
